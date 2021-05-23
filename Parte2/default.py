297ac525ecce9d744412721c1fd5db9fd77c8c81
Adrian Diaz <adriandiazag@gmail.com>
2021-05-23 19:19:10 +0200

    Modificaciones

--- Parte2/default.py
+++ Parte2/default.py
@@ -12,7 +12,7 @@ session=requests.session()
 
 @app.route('/', methods=["GET"])
 def inicio():
-    r=session.get(webscan_base+'host/search?&query=all',params=payload)
+    r=session.get(webscan_base+'host/search?&query=country:es city:"Sevilla" port:"3389" has_screenshot:1 -screenshot.label:blank',params=payload)
     response=r.json()
     ips=[]
     for i in response["matches"]:
@@ -22,6 +22,7 @@ def inicio():
                 ips.remove(ip)
 
     randomip=random.choice(ips)
+    ipinfo=session.get(webscan_base+'host/'+randomip,params=payload)
 
     cam=session.get(webscan_base+'host/search?query=port:554 country:es has_screenshot:1 -screenshot.label:blank',params=payload)
     respuesta=cam.json()
@@ -31,7 +32,10 @@ def inicio():
             ipcam.append(ip_list)
 
     randomcamip1=random.choice(ipcam)
+    ipinfo1=session.get(webscan_base+'host/'+randomcamip1,params=payload)
+    
     randomcamip2=random.choice(ipcam)
-    return render_template('inicio.html', randomip=randomip, randomcamip=randomcamip1, randomcamip2=randomcamip2)
+    ipinfo2=session.get(webscan_base+'host/'+randomcamip2,params=payload)
+    return render_template('inicio.html', randomip=randomip, answer=ipinfo.json(), randomcamip=randomcamip1, randomcamip2=randomcamip2, answer1=ipinfo1.json(), answer2=ipinfo2.json())
 
 app.run(debug=True)
