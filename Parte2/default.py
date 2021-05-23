dd940e72b5d2ca18f9d04ae21d0bf98e10fdf009
Adrian Diaz <adriandiazag@gmail.com>
2021-05-23 14:34:43 +0200

    Modificaciones

--- Parte2/default.py
+++ Parte2/default.py
@@ -1,15 +1,37 @@
 from flask import Flask, render_template,abort,request
 import requests
 import os
+import random
 
 app = Flask(__name__)
 key=os.environ["ShodanKey"]
 payload={'key':key}
 webscan_base="https://api.shodan.io/shodan/"
 vuln_base="https://exploits.shodan.io/api/search?query="
+session=requests.session()
 
 @app.route('/', methods=["GET"])
 def inicio():
-    return render_template('inicio.html')
+    r=session.get(webscan_base+'host/search?&query=all',params=payload)
+    response=r.json()
+    ips=[]
+    for i in response["matches"]:
+        ips.append(i["ip_str"])
+        for ip in ips:
+            if ip.startswith('2600'):
+                ips.remove(ip)
+
+    randomip=random.choice(ips)
+
+    cam=session.get(webscan_base+'host/search?query=port:554 country:es has_screenshot:1 -screenshot.label:blank',params=payload)
+    respuesta=cam.json()
+    ipcam=[]
+    for camera in respuesta["matches"]:
+            ip_list=camera["ip_str"]
+            ipcam.append(ip_list)
+
+    randomcamip1=random.choice(ipcam)
+    randomcamip2=random.choice(ipcam)
+    return render_template('inicio.html', randomip=randomip, randomcamip=randomcamip1, randomcamip2=randomcamip2)
 
 app.run(debug=True)
