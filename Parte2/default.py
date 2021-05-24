f4802750ba352e2580e001558bef99cc424a4bf4
Adrian Diaz <adriandiazag@gmail.com>
2021-05-24 13:03:58 +0200

    Modificaciones

--- Parte2/default.py
+++ Parte2/default.py
@@ -3,6 +3,7 @@ import requests
 import os
 import random
 import re
+import pdb
 
 app = Flask(__name__)
 key=os.environ["ShodanKey"]
@@ -14,18 +15,19 @@ session=requests.session()
 @app.route('/', methods=["GET"])
 def inicio():
     r=session.get(webscan_base+'host/search?&query=country:es city:"Sevilla" port:"3389" has_screenshot:1 -screenshot.label:blank',params=payload)
+    r.raw.chunked=True
+    r.encoding='utf-8'
     response=r.json()
     ips=[]
     for i in response["matches"]:
         ips.append(i["ip_str"])
-        for ip in ips:
-            if ip.startswith('2600'):
-                ips.remove(ip)
 
     randomip=random.choice(ips)
     ipinfo=session.get(webscan_base+'host/'+randomip,params=payload)
 
     cam=session.get(webscan_base+'host/search?query=port:554 country:es has_screenshot:1 -screenshot.label:blank',params=payload)
+    cam.raw.chunked=True
+    cam.encoding='utf-8'
     respuesta=cam.json()
     ipcam=[]
     for camera in respuesta["matches"]:
@@ -39,7 +41,7 @@ def inicio():
     ipinfo2=session.get(webscan_base+'host/'+randomcamip2,params=payload)
     return render_template('inicio.html', randomip=randomip, answer=ipinfo.json(), randomcamip=randomcamip1, randomcamip2=randomcamip2, answer1=ipinfo1.json(), answer2=ipinfo2.json())
 
-@app.route('/hostscan', methods=["GET","POST"])
+@app.route('/hostscan/', methods=["GET","POST"])
 def hostscan():
     filter=session.get("https://beta.shodan.io/search/filters").text
     query=re.findall(r'<li>(.*?)</li>',str(filter))
